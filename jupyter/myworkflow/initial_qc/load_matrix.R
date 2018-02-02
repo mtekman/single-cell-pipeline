f210495d1dbe3b7a5831556f76d848101c4d9347
Mehmet Tekman <m.tekman@ucl.ac.uk>
2018-02-02 16:33:20 +0000

    updated step 2

--- jupyter/myworkflow/initial_qc/load_matrix.R
+++ jupyter/myworkflow/initial_qc/load_matrix.R
@@ -3,76 +3,95 @@ library(scater)
 
 # Set the raw data and save/load it
 umi <- NULL
-generateit = F
+if (!exists("generate_matrix")){
+   generate_matrix = T
+}
 
-genes_of_interest <- c(
-    "Eomes", "Brachyury", "Mesp1",                # meso
-    "Pou5f1", "nanog",                            # pluripotent
-    "Sox1", "Sox2", "Pou3f1", "zfp462", "slc7a3"  # neuroectoderm
-) 
+if (!exists("matrix_destination")){
+   matrix_destination = "matrix.rds"
+}
 
-if (is.null(umi) && generateit){
+
+#genes_of_interest <- c(
+#    "Eomes", "Brachyury", "Mesp1",                # meso
+#    "Pou5f1", "nanog",                            # pluripotent
+#    "Sox1", "Sox2", "Pou3f1", "zfp462", "slc7a3"  # neuroectoderm
+#) 
+
+if (is.null(umi) && generate_matrix){
+    message("Regenerating matrix from source file ", matrix_destination ,"...")
     counts_raw <- read.table("/extra/sebastian_arnolds/analysis/trim/counts_matrix.tsv",sep="\t")
     barcodes_vector <- as.vector(
         read.table("/extra/sebastian_arnolds/analysis/celseq_barcodes_raw_all.txt")[, 1]
     )
     count_matrix <- as.matrix(counts_raw[, barcodes_vector])
 
-    # Change labels for genes of interest
-    name_map = list(
-        ENSMUSG00000062327 = "Brachyury",
-        ENSMUSG00000032446 = "Eomes",
-        ENSMUSG00000030544 = "Mesp1",
-        ENSMUSG00000024406 = "Pou5f1",
-        ENSMUSG00000012396 = "nanog",
-        ENSMUSG00000096014 = "Sox1",
-        ENSMUSG00000074637 = "Sox2",
-        ENSMUSG00000090125 = "Pou3f1",
-        ENSMUSG00000060206 = "zfp462",
-        ENSMUSG00000031297 = "slc7a3"
-    )
+#    # Change labels for genes of interest
+#    name_map = list(
+#        ENSMUSG00000062327 = "Brachyury",
+#        ENSMUSG00000032446 = "Eomes",
+#        ENSMUSG00000030544 = "Mesp1",
+#        ENSMUSG00000024406 = "Pou5f1",
+#        ENSMUSG00000012396 = "nanog",
+#        ENSMUSG00000096014 = "Sox1",
+#        ENSMUSG00000074637 = "Sox2",
+#        ENSMUSG00000090125 = "Pou3f1",
+#        ENSMUSG00000060206 = "zfp462",
+#        ENSMUSG00000031297 = "slc7a3"
+#    )
+#
+#    for (i in 1:length(name_map)){
+#        old_gene_name <- names(name_map[i])
+#        new_gene_name <- name_map[[i]]
+#        #message(old_gene_name, "→", new_gene_name)
+#        rownames(count_matrix)[which(rownames(count_matrix) == old_gene_name)] <- new_gene_name
+#    }
 
-    for (i in 1:length(name_map)){
-        old_gene_name <- names(name_map[i])
-        new_gene_name <- name_map[[i]]
-        #message(old_gene_name, "→", new_gene_name)
-        rownames(count_matrix)[which(rownames(count_matrix) == old_gene_name)] <- new_gene_name
-    }
-
-    saveRDS(count_matrix, "matrix.rds")
     umi <- count_matrix
 
+    # Create SCE and annotate
+    sce <- SingleCellExperiment(assays = list(counts = umi))
+
+    # Assign known/related groups
+    #is.meso <- rownames(sce) %in% c("Eomes", "Brachyury", "Mesp1")
+    #is.pluri <- rownames(sce) %in% c("Pou5f1", "nanog")
+    #is.neuro <- rownames(sce) %in% c("Sox1", "Sox2", "Pou3f1", "zfp462", "slc7a3")
+    #rowData(sce)$is_mesoderm <- is.meso
+    #rowData(sce)$is_pluripotenz <- is.pluri
+    #rowData(sce)$is_neuroectoderm <- is.neuro
+
+    # Assign known/related cell batches
+    colData(sce)$plate_number <- c(rep("plate1", 96), rep("plate2", 96)) 
+
+    #head(counts(sce))
+
+    sce <- getBMFeatureAnnos(
+        sce, 
+        filters = "ensembl_gene_id",
+        attributes = c(
+            "ensembl_gene_id",              # Gene stable ID
+            "external_gene_name",           # Casual name
+            "external_transcript_name",     # Transcript-specific name
+            "gene_biotype",                 # Gene biotype
+            "transcript_biotype",           # Trans type
+            "description",                  # Gene description
+            "band",                         # Karyotype band
+            "refseq_mrna"        
+        ),
+        feature_symbol = "mgi_symbol",
+        feature_id = "ensembl_gene_id",
+        biomart = "ENSEMBL_MART_ENSEMBL",
+        dataset = "mmusculus_gene_ensembl", 
+        host = "www.ensembl.org"
+    )
+
+    saveRDS(sce, matrix_destination)
+
 } else {
-    umi <- as.matrix(readRDS("matrix.rds"))
+    message("Loading matrix from source file ", matrix_destination, "...")
+    sce <- readRDS(matrix_destination)
 }
 
-# Create SCE and annotate
-sce <- SingleCellExperiment(assays = list(counts = umi))
-
-# Assign known/related groups
-is.meso <- rownames(sce) %in% c("Eomes", "Brachyury", "Mesp1")
-is.pluri <- rownames(sce) %in% c("Pou5f1", "nanog")
-is.neuro <- rownames(sce) %in% c("Sox1", "Sox2", "Pou3f1", "zfp462", "slc7a3")
-
-rowData(sce)$is_mesoderm <- is.meso
-rowData(sce)$is_pluripotenz <- is.pluri
-rowData(sce)$is_neuroectoderm <- is.neuro
-
-# Assign known/related cell batches
-colData(sce)$plate_number <- c(rep("plate1", 96), rep("plate2", 96)) 
-
-#head(counts(sce))
-
-#sce <- getBMFeatureAnnos(
-#    sce, 
-#    filters = "ensembl_gene_id", 
-#    attributes = c("ensembl_gene_id", "external_gene_name"), 
-#    feature_symbol = "mgi_symbol",
-#    feature_id = "ensembl_gene_id",
-#    biomart = "ENSEMBL_MART_ENSEMBL",
-#    dataset = "mmusculus_gene_ensembl", 
-#    host = "www.ensembl.org"
-#)
 
 message(dim(sce)[1], " genes x ", dim(sce)[2], " cells. (", 
         length(unique(colnames(sce))), ") unique barcodes.")
